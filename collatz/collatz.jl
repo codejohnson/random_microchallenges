7fbc81da6cc09740d825b638db7c8f209e39dd82
Jorge Johnson <jorge.johnson.code@gmail.com>
2021-05-09 17:02:37 -0500

    Update collatz.jl

--- collatz/collatz.jl
+++ collatz/collatz.jl
@@ -1,10 +1,14 @@
-function max_collantz_speed(a,b)
-    ms = (0,0)
-    next = (x) -> x % 2 == 1 ? 3 * x + 1 : x / 2 
-    speed = (s) -> s == 1 ? 0 : speed(next(s)) + 1      
-    for i in a:b+1
-        s= speed(i)
-        if s > ms[2] ms = (i,s) end
+function typed_flat_collantz(b::Int64)::Tuple{Int64,Int64}
+    ms::Tuple{Int64,Int64} = (0,0)
+    while b > 0
+        n::Int64 = b
+        c::Int64 = 0
+        while n > 1
+            c += 1
+            n = if n % 2 == 1  3 * n + 1 else n / 2 end
+        end
+        if c > ms[2] ms = (b,c) end
+        b -= 1
     end
     ms
 end
